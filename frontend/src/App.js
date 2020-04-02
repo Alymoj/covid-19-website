2a41b41f30aa927f676083f5b27d6f291b64adc7
Muhammad Aly <mohamed97010@stemegypt.edu.eg>
2020-04-02 18:56:16 -0500

    project set-up and initial page layout

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Page from "./components/Page";
+import Body from "./components/Body";
+import './App.css';
+
+function App() {
+  return (
+      <Page>
+        <Body />
+      </Page>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Body.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Body = () => (<p>This is Body.js</p>);
+
+export default Body;
